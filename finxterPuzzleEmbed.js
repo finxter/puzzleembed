21b295f223086871148b0e84d2eed1120396b7ec
finxter <57671493+finxter@users.noreply.github.com>
2019-11-28 16:23:51 +0100

    Update finxterPuzzleEmbed.js

--- finxterPuzzleEmbed.js
+++ finxterPuzzleEmbed.js
@@ -1,43 +1,178 @@
-function getPuzzle() {
-	var puzzles = [[0, "print('hello world')"], 
-		       [1, "print('Finxter is cool')"]];
-	var id = Math.floor(Math.random();
-	return puzzles[id * puzzles.length)];
-}
-
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	// Load Puzzle
-	var finxter_puzzle = document.createElement('div');       
+	// Load General Stuff
+	var finxter_puzzle = document.createElement('div');  
+	var finxter_header = document.createElement('h1');
+	var finxter_code = document.createElement('div');
+	
 	document.body.appendChild(finxter_puzzle);
+	finxter_puzzle.appendChild(finxter_header);
+	finxter_puzzle.appendChild(finxter_code);
 
 	finxter_puzzle.width = '300';
 	finxter_puzzle.height = '300';
-	finxter_puzzle.innerHTML = '<h1 id="Title">Daily Finxter Puzzle</h1>';
+	finxter_header.innerHTML = '<h1 id="Title">Daily Finxter Puzzle</h1>';
 	
-	var res1 = getPuzzle(); // [id, puzzle_string]
-	
-	finxter_puzzle.innerHTML += '<pre>' + res1[1] + '</pre>';
-
 	// Create Solution Button
 	var buttonSolution = document.createElement('button');
-    	document.body.appendChild(buttonSolution);
-    
-	buttonSolution.innerHTML = 'Check Solution';
-	buttonSolution.style.width = '125';
+    document.body.appendChild(buttonSolution);
+	
+	buttonSolution.style.width = '200';
 	buttonSolution.style.height = '30';
 	buttonSolution.style.fontSize = '20px';
 	buttonSolution.style.color = 'white';
 	buttonSolution.style.background = 'black';
+	buttonSolution.innerHTML = 'Check Solution';
 	
+	// Load Puzzle
+	logic(finxter_puzzle, buttonSolution);
 	
+});
+
+function logic(finxter_code, buttonSolution) {
 	
+	var res1 = getPuzzle(); // [id, puzzle_string]
+	
+	finxter_code.innerHTML = '<pre>' + res1[1] + '</pre>';
+
 	
 	buttonSolution.onclick = function() {
-    			window.open("https://app.finxter.com/learn/computer/science/" + res1[0], "_blank");
-    		}
-	
-	
+		window.open("https://app.finxter.com/learn/computer/science/" + res1[0], "_blank");
+		logic(finxter_code, buttonSolution);
+    }
+}
 
-});
+function getPuzzle() {
+	var puzzles = [[514, `xs = [[1, 2], [3, 4]]
+ys = list(xs)
+
+print(xs[0] is ys[0])`],
+[513, `xs = [[1, 2], [3, 4]]
+ys = list(xs)
+
+print(xs is ys)`],
+[512, `def srange(x):
+    return range(x-3, x)
+    
+s = 0
+for i in srange(2):
+    s += i
+    
+print(s)`],
+[511, `string = 'ppy!'
+fruit = 'a'.join(list(string))
+
+print(fruit)`],
+[510, `python = ['cool']
+x = python in python
+
+print(x)`],
+[509, `nums = [1, 2, 3]
+nums.append(nums[:])
+
+print(len(nums))`],
+[508, `s = 'terfinx'
+s = ''.join(sorted(list(s)[3:7])+list(s)[0:3])
+
+print(s)`],
+[507, `def add(a=0, b=1):
+    return a + b
+    
+
+print(add(add(add())))`],
+[506, `def avg(x, *xs):
+    if len(xs) == 0:
+        return x
+    s = x + sum(xs)
+    n = 1 + len(xs)
+    return s/n
+ 
+print(avg(2, 4, 6) == avg(2, 4, 6, 6, 2))`],
+[505, `l = [[]]
+
+if l:
+    print(True)
+else:
+    print(False)`],
+[503, `xs = [-1, 0, 1, 2, 3]
+
+l = [x + 1 for x in xs]
+print(sum(l))`],
+[502, `xs = [1, 5, 3, 7, 2, 8, 4, 6]
+y = 0
+
+for x in xs:
+    if x < y:
+        y = x
+
+print(min(xs) == y)`],
+[501, `xs = [1, 5, 3, 7, 2, 8, 4, 6]
+y = xs[0]
+
+for x in xs:
+    if x > y:
+        y = x
+
+print(max(xs) == y)`],
+[500, `a = 'hello'
+b = 'world'
+
+print(a, b, sep=' Python ', end='!')`],
+[498, `def len_(x):
+    length = len(x)
+    # print everything in same row
+    print(length, end='')
+    return length
+    
+items = [3.14, 'moon', {}] 
+i = 0
+
+while i < len_(items):
+    i += 1`],
+[494, `item_prices = [
+   ('apple', 0.45),
+   ('banana', 0.75),
+   ('kiwi', 0.5),
+   ('orange', 0.8)
+]
+
+prices = [price for item, price in item_prices] 
+
+print(sum(prices))`],
+[490, `speeds = [
+    ('car', 180.0),
+    ('bike', 20.5),
+    ('boat', 55.3),
+    ('ski', 31.8),
+]
+
+fastest = max(speeds)
+
+print(fastest[0])`],
+[487, `fs = [
+    lambda x: x + 1,
+    lambda x: x + x,
+    lambda x: x * x,
+]
+
+n = 0
+
+for f in fs:
+    n = f(n)
+    
+print(n)`],
+[486, `my_tuple = 1, 1, 1 == (1, 1, 1)
+
+print(my_tuple)`],
+[473, `a = 1 > 0
+
+if a or (1 / 0 == 0):
+    print('ok')
+else:
+    print('nok')`]
+
+];
+	var id = Math.floor(Math.random() * puzzles.length);
+	return puzzles[id];
+}
